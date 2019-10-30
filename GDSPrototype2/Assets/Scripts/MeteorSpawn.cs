2c6f711b7419fe817bea8cca374452534ec772e3
Bryce <1013537@student.sae.edu.au>
2019-10-30 20:40:09 +1100

    meteor spawner, tried to do phone stuff and failed
    -Made a spawner for meteors that goes off on a timer
    -tried to do stuff for android phone, switched the project to android but was unable to do anything

---
+++ GDSPrototype2/Assets/Scripts/MeteorSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorSpawn : MonoBehaviour
+{
+    public float timerspeed = 2f;
+    private float elapsed;
+
+    public GameObject meteorPrefab;
+    GameObject meteorPrefabClone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        elapsed += Time.deltaTime;
+        if(elapsed >= timerspeed)
+        {
+            elapsed = 0f;
+        meteorPrefabClone = Instantiate(meteorPrefab, transform.position, Quaternion.identity) as GameObject;
+        Destroy(meteorPrefabClone, 3);
+        }
+    }
+}
