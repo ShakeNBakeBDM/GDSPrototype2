c1c7bfaf77d8c879b04dda6b0c9cc932c5a6098a
Bryce <1013537@student.sae.edu.au>
2019-10-30 16:15:15 +1100

    Started Obstacles and finishline
    - Now when the players collider hits an objects collider with the tag Obstacle assigned to it the player will be sent to the start of the race.
    - When the player reaches the finish line which has the tag finish on it they will be sent to the start for now also

---
+++ GDSPrototype2/Assets/Scripts/FinishL.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishL : MonoBehaviour
+{
+
+    private Vector3 startPos;
+
+    private void Awake()
+    {
+        //setting the startPos as the transform of the player when the game starts
+        startPos = transform.position;
+    }
+    void OnTriggerEnter2D(Collider2D finishL)
+    {
+        
+        if (finishL.tag == "Finish")
+        transform.position = startPos;
+        Debug.Log("Finish");
+    }
+}
---
+++ GDSPrototype2/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    //the players start position stored as a V3
+    private Vector3 startPos;
+
+    private void Awake()
+    {
+        //setting the startPos as the transform of the player when the game starts
+        startPos = transform.position;
+    }
+
+    void OnTriggerEnter2D(Collider2D colliderI)
+    {
+        //if the players collider hits objects with the tag Obstacle
+        //it will then send the player back to the startPos
+        if (colliderI.tag == "Obstacle")
+            transform.position = startPos;   
+            Debug.Log("Reset");
+    }
+    
+}
