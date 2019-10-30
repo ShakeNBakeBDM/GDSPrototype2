551dcd6e2f190fa629e49bd2a016e6af06094a4c
Jake <1012334@student.sae.edu.au>
2019-10-30 20:58:20 +1100

    Added menu & build order

---
+++ GDSPrototype2/Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        //loads the first scene in the build order after the main menu
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
