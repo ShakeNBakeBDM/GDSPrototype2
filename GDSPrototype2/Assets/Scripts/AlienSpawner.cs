aa52850d1bed7e7850caf09f7fa851af10521833
Bryce <1013537@student.sae.edu.au>
2019-10-30 21:54:19 +1100

    Alien Spawner
    _spawn aliens from the right side of the screen sending them to the left

---
+++ GDSPrototype2/Assets/Scripts/AlienSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienSpawner : MonoBehaviour
+{
+    public float timerspeed = 2f;
+    private float elapsed;
+
+    public GameObject alienPrefab;
+    GameObject alienPrefabClone;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        elapsed += Time.deltaTime;
+        if (elapsed >= timerspeed)
+        {
+            elapsed = 0f;
+            alienPrefabClone = Instantiate(alienPrefab, transform.position, Quaternion.identity) as GameObject;
+            Destroy(alienPrefabClone, 3);
+        }
+    }
+}
